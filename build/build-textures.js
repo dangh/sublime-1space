75ccd452e469cfa7fdab042e99b60c069604e019
Dang <huynhminhdang@gmail.com>
2020-07-12 03:56:06 +0800

    generate rouned rectangle textures using ejs template

--- build/build-textures.js
+++ build/build-textures.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const fs = require('fs').promises;
+const deepmerge = require('deepmerge');
 const YAML = require('js-yaml');
+const EJS = require('ejs')
 const SVG = require('./svg');
 const log = require('./log');
 const { rootDir } = require('./path');
@@ -10,6 +12,14 @@ main().then(log.error);
 
 async function main() {
   let textures = (await YAML.load(await fs.readFile('./textures.yml', 'utf8')));
+
+  //resolve refs
+  for(let [name, texture] of Object.entries(textures)) {
+    if(texture.ref) {
+      let ref = textures[texture.ref];
+      textures[name] = deepmerge(ref, texture);
+    }
+  }
 
   let texturesToBuild = process.argv.slice(2);
   if(texturesToBuild.length == 0) texturesToBuild = Object.keys(textures);
@@ -21,6 +31,7 @@ async function main() {
     if(texture.startsWith('$')) continue;
     if(!width) throw new Error(`Invalid texture metadata: \`width\` is missing for \`${texture}\``);
     if(!height) height = width;
+    if(svg.raw && svg.variables) svg.raw = EJS.render(svg.raw, svg.variables, { _with: true });
     tasks[texture] = {
       input: svg,
       outputs: {
